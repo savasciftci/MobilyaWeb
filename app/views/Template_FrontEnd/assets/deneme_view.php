08a9c168a839f891b4272e1c1310674a5a00f19a
aytacslmn <Ayta@Ayta-PC>
2015-12-20 15:59:20 +0200

    son hali

---
+++ app/views/Template_FrontEnd/assets/deneme_view.php
@@ -0,0 +1,387 @@
+
+<!-- Start HomePage Slider -->
+
+<section id="home">	
+  <!-- Carousel -->
+  <div id="main-slide" class="carousel slide" data-ride="carousel">
+
+     <!-- Indicators -->
+     <ol class="carousel-indicators">
+        <li data-target="#main-slide" data-slide-to="0" class="active"></li>
+        <li data-target="#main-slide" data-slide-to="1"></li>
+        <li data-target="#main-slide" data-slide-to="2"></li>
+    </ol><!--/ Indicators end-->
+
+    <!-- Carousel inner -->
+    <div class="carousel-inner">
+     <div class="item active">
+        <img class="img-responsive" src="<?php echo SITE_FRONT_ASSETS_DISTIMG; ?>/slider/bg1.jpg" alt="slider">
+<!--        <div class="slider-content">
+          <div class="col-md-12 text-center">
+            <h2 class="animated2">
+              <span>Welcome to <strong>Margo</strong></span>
+          </h2>
+          <h3 class="animated3">
+           <span>The ultimate aim of your business</span>
+       </h3>
+       <p class="animated4"><a href="#" class="slider btn btn-system btn-large">Check Now</a></p>
+   </div>
+</div>-->
+</div><!--/ Carousel item end -->
+<div class="item">
+    <img class="img-responsive" src="<?php echo SITE_FRONT_ASSETS_DISTIMG; ?>/slider/bg2.jpg" alt="slider">
+<!--  <div class="slider-content">
+    <div class="col-md-12 text-center">
+      <h2 class="animated4">
+        <span><strong>Margo</strong> for the highest</span>
+    </h2>
+    <h3 class="animated5">
+     <span>The Key of your Success</span>
+ </h3>		
+ <p class="animated6"><a href="#" class="slider btn btn-system btn-large">Buy Now</a></p>	     
+</div>
+</div>-->
+</div><!--/ Carousel item end -->
+<div class="item">
+  <img class="img-responsive" src="<?php echo SITE_FRONT_ASSETS_DISTIMG; ?>/slider/bg3.jpg" alt="slider">
+<!--  <div class="slider-content">
+    <div class="col-md-12 text-center">
+      <h2 class="animated7 white">
+        <span>The way of <strong>Success</strong></span>
+    </h2>
+    <h3 class="animated8 white">
+     <span>Why you are waiting</span>
+ </h3>		
+ <div class="">
+  <a class="animated4 slider btn btn-system btn-large btn-min-block" href="#">Get Now</a><a class="animated4 slider btn btn-default btn-min-block" href="#">Live Demo</a>
+</div>     
+</div>
+</div>-->
+</div><!--/ Carousel item end -->
+</div><!-- Carousel inner end-->
+
+<!-- Controls -->
+<a class="left carousel-control" href="#main-slide" data-slide="prev">
+   <span><i class="fa fa-angle-left"></i></span>
+</a>
+<a class="right carousel-control" href="#main-slide" data-slide="next">
+   <span><i class="fa fa-angle-right"></i></span>
+</a>
+
+</div><!-- /carousel -->    	
+</section>
+<!-- End HomePage Slider -->
+
+
+
+<!-- Start Content -->
+<div id="content">
+   <div class="container">
+      
+      <!-- Start Services Icons -->
+      <div class="row">
+        
+        <!-- Start Service Icon 1 -->
+        <div class="col-md-4 col-sm-6 service-box service-center">
+           <div class="service-boxed">
+              <div class="service-icon" style="margin-top:-25px;">
+                <i class="fa fa-magic icon-medium-effect icon-effect-2"></i>
+            </div>
+            <div class="service-content">
+                <h4>High Quality Theme</h4>
+                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem Ipsum is simply dummy text of the printing.</p>
+            </div>
+        </div>
+    </div>
+    <!-- End Service Icon 1 -->
+    
+    <!-- Start Service Icon 2 -->
+    <div class="col-md-4 col-sm-6 service-box service-center">
+     <div class="service-boxed">
+        <div class="service-icon" style="margin-top:-25px;">
+          <i class="fa fa-eye icon-medium-effect icon-effect-2"></i>
+      </div>
+      <div class="service-content">
+          <h4>Retina Display Ready</h4>
+          <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem Ipsum is simply dummy text of the printing.</p>
+      </div>
+  </div>
+</div>
+<!-- End Service Icon 2 -->
+
+<!-- Start Service Icon 3 -->
+<div class="col-md-4 col-sm-6 service-box service-center">
+   <div class="service-boxed">
+      <div class="service-icon" style="margin-top:-25px;">
+        <i class="fa fa-code icon-medium-effect icon-effect-1"></i>
+    </div>
+    <div class="service-content">
+        <h4>Clean Modern Code</h4>
+        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem Ipsum is simply dummy text of the printing.</p>
+    </div>
+</div>
+</div>
+<!-- End Service Icon 3 -->
+
+<!-- Start Service Icon 4 -->
+<!--<div class="col-md-4 col-sm-6 service-box service-center">
+   <div class="service-boxed">
+      <div class="service-icon" style="margin-top:-25px;">
+        <i class="fa fa-rocket icon-medium-effect icon-effect-1"></i>
+    </div>
+    <div class="service-content">
+        <h4>Fast & Light Theme</h4>
+        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem Ipsum is simply dummy text of the printing.</p>
+    </div>
+</div>
+</div>-->
+<!-- End Service Icon 4 -->
+
+</div>
+<!-- End Services Icons -->
+
+<!-- Divider -->
+<div class="hr1 margin-top"></div>
+
+     
+       <!-- Start Recent Projects Carousel -->
+        
+                               
+                                            
+
+                
+                
+                
+                
+
+               
+
+               
+
+                
+              
+                
+                
+                
+                
+                
+                <!-- Divider -->
+                <div class="hr1 margin-60"></div>
+            </div>
+            
+            
+                
+                
+                <!-- Divider -->
+                <div class="hr1 margin-60"></div>
+            </div>
+            
+            
+            
+        
+        
+        
+        
+        
+       <!-- Start Recent Projects Carousel -->
+        
+
+				<div class="recent-projects">
+					<h4 class="title"><span>Ürünlerimiz</span></h4>
+					<div class="projects-carousel touch-carousel">
+						
+					
+						
+						<div class="portfolio-item item">
+							<div class="portfolio-border">
+								<div class="portfolio-thumb">
+									<a class="lightbox" title="This is an image title" href="<?php echo SITE_FRONT_ASSETS_DISTIMG; ?>/Modalife-Vestiyer-Modelleri3.jpg">
+										<div class="thumb-overlay"><i class="fa fa-arrows-alt"></i></div>
+										<img alt="" src="<?php echo SITE_FRONT_ASSETS_DISTIMG; ?>/Portfolio-1/images.jpg" />
+									</a>
+								</div>
+								<div class="portfolio-details">
+									<a href="#">
+										<h4>Vestiyer V07 Sürgülü Kapak</h4>
+									</a>
+								</div>
+							</div>
+						</div>
+						
+						<div class="portfolio-item item">
+							<div class="portfolio-border">
+								<div class="portfolio-thumb">
+									<a class="lightbox" title="This is an image title" href="<?php echo SITE_FRONT_ASSETS_DISTIMG; ?>/Modalife-Vestiyer-Modelleri3.jpg">
+										<div class="thumb-overlay"><i class="fa fa-arrows-alt"></i></div>
+										<img alt="" src="<?php echo SITE_FRONT_ASSETS_DISTIMG; ?>/Portfolio-1/images.jpg" />
+									</a>
+								</div>
+								<div class="portfolio-details">
+									<a href="#">
+										<h4>Lorem Ipsum Dolor asdaaaaaa</h4>
+									</a>
+								</div>
+							</div>
+						</div>
+						
+						<div class="portfolio-item item">
+							<div class="portfolio-border">
+								<div class="portfolio-thumb">
+									<a class="lightbox" title="This is an image title" href="<?php echo SITE_FRONT_ASSETS_DISTIMG; ?>/Modalife-Vestiyer-Modelleri3.jpg">
+										<div class="thumb-overlay"><i class="fa fa-arrows-alt"></i></div>
+										<img alt="" src="<?php echo SITE_FRONT_ASSETS_DISTIMG; ?>/Portfolio-1/images.jpg" />
+									</a>
+								</div>
+								<div class="portfolio-details">
+									<a href="#">
+										<h4>Lorem Ipsum Dolor</h4>
+									</a>
+								</div>
+							</div>
+						</div>
+						
+						<div class="portfolio-item item">
+							<div class="portfolio-border">
+								<div class="portfolio-thumb">
+									<a class="lightbox" title="This is an image title" href="<?php echo SITE_FRONT_ASSETS_DISTIMG; ?>/Modalife-Vestiyer-Modelleri3.jpg">
+										<div class="thumb-overlay"><i class="fa fa-arrows-alt"></i></div>
+										<img alt="" src="<?php echo SITE_FRONT_ASSETS_DISTIMG; ?>/Portfolio-1/images.jpg" />
+									</a>
+								</div>
+								<div class="portfolio-details">
+									<a href="#">
+										<h4>Lorem Ipsum Dolor</h4>
+									</a>
+								</div>
+							</div>
+						</div>
+						
+						<div class="portfolio-item item">
+							<div class="portfolio-border">
+								<div class="portfolio-thumb">
+									<a class="lightbox" title="This is an image title" href="<?php echo SITE_FRONT_ASSETS_DISTIMG; ?>/Modalife-Vestiyer-Modelleri3.jpg">
+										<div class="thumb-overlay"><i class="fa fa-arrows-alt"></i></div>
+										<img alt="" src="<?php echo SITE_FRONT_ASSETS_DISTIMG; ?>/Portfolio-1/images.jpg" />
+									</a>
+								</div>
+								<div class="portfolio-details">
+									<a href="#">
+										<h4>Lorem Ipsum Dolor</h4>
+								</div>
+							</div>
+						</div>
+						
+						<div class="portfolio-item item">
+							<div class="portfolio-border">
+								<div class="portfolio-thumb">
+									<a class="lightbox" title="This is an image title" href="<?php echo SITE_FRONT_ASSETS_DISTIMG; ?>/Modalife-Vestiyer-Modelleri3.jpg">
+										<div class="thumb-overlay"><i class="fa fa-arrows-alt"></i></div>
+										<img alt="" src="<?php echo SITE_FRONT_ASSETS_DISTIMG; ?>/Portfolio-1/images.jpg" />
+									</a>
+								</div>
+								<div class="portfolio-details">
+									<a href="#">
+										<h4>Lorem Ipsum Dolor</h4>
+									</a>
+								</div>
+							</div>
+						</div>
+						
+						<div class="portfolio-item item">
+							<div class="portfolio-border">
+								<div class="portfolio-thumb">
+									<a class="lightbox" title="This is an image title" href="<?php echo SITE_FRONT_ASSETS_DISTIMG; ?>/Modalife-Vestiyer-Modelleri3.jpg">
+										<div class="thumb-overlay"><i class="fa fa-arrows-alt"></i></div>
+										<img alt="" src="<?php echo SITE_FRONT_ASSETS_DISTIMG; ?>/Portfolio-1/images.jpg" />
+									</a>
+								</div>
+								<div class="portfolio-details">
+									<a href="#">
+										<h4>Lorem Ipsum Dolor</h4>
+									</a>
+								</div>
+							</div>
+						</div>
+                        
+                        <div class="portfolio-item item">
+							<div class="portfolio-border">
+								<div class="portfolio-thumb">
+									<a class="lightbox" title="This is an image title" href="<?php echo SITE_FRONT_ASSETS_DISTIMG; ?>/Modalife-Vestiyer-Modelleri3.jpg">
+										<div class="thumb-overlay"><i class="fa fa-arrows-alt"></i></div>
+										<img alt="" src="<?php echo SITE_FRONT_ASSETS_DISTIMG; ?>/Portfolio-1/images.jpg" />
+									</a>
+								</div>
+								<div class="portfolio-details">
+									<a href="#">
+										<h4>Lorem Ipsum Dolor</h4>
+									</a>
+								</div>
+							</div>
+						</div>
+                        
+                       <div class="portfolio-item item">
+							<div class="portfolio-border">
+								<div class="portfolio-thumb">
+									<a class="lightbox" title="This is an image title" href="<?php echo SITE_FRONT_ASSETS_DISTIMG; ?>/Modalife-Vestiyer-Modelleri3.jpg">
+										<div class="thumb-overlay"><i class="fa fa-arrows-alt"></i></div>
+										<img alt="" src="<?php echo SITE_FRONT_ASSETS_DISTIMG; ?>/Portfolio-1/images.jpg" />
+									</a>
+								</div>
+								<div class="portfolio-details">
+									<a href="#">
+										<h4>Lorem Ipsum Dolor</h4>
+									</a>
+								</div>
+							</div>
+						</div>
+                        
+                       <div class="portfolio-item item">
+							<div class="portfolio-border">
+								<div class="portfolio-thumb">
+									<a class="lightbox" title="This is an image title" href="<?php echo SITE_FRONT_ASSETS_DISTIMG; ?>/Modalife-Vestiyer-Modelleri3.jpg">
+										<div class="thumb-overlay"><i class="fa fa-arrows-alt"></i></div>
+										<img alt="" src="<?php echo SITE_FRONT_ASSETS_DISTIMG; ?>/Portfolio-1/images.jpg" />
+									</a>
+								</div>
+								<div class="portfolio-details">
+									<a href="#">
+										<h4>Lorem Ipsum Dolor</h4>
+									</a>
+								</div>
+							</div>
+						</div>
+                        
+                        <div class="portfolio-item item">
+							<div class="portfolio-border">
+								<div class="portfolio-thumb">
+									<a class="lightbox" title="Lorem Ipsum Dolor" href="<?php echo SITE_FRONT_ASSETS_DISTIMG; ?>/Modalife-Vestiyer-Modelleri3.jpg">
+										<div class="thumb-overlay"><i class="fa fa-arrows-alt"></i></div>
+										<img alt="" src="<?php echo SITE_FRONT_ASSETS_DISTIMG; ?>/Portfolio-1/images.jpg" />
+									</a>
+								</div>
+								<div class="portfolio-details">
+									<a href="#">
+										<h4>Lorem Ipsum Dolor</h4>
+									</a>
+								</div>
+							</div>
+						</div>
+						
+					</div>
+				</div>
+				<!-- End Recent Projects Carousel -->
+        
+        
+        
+            
+            
+            
+        </div>
+    </div>
+    <!-- End Content -->
+
+<!-- Divider -->
+<div class="hr1 margin-60"></div>
+</div>
+</div>
+<!-- End Content -->
---
+++ app/views/Template_FrontEnd/assets/header_view.php
@@ -0,0 +1,197 @@
+
+<!doctype html>
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><html lang="en" class="no-js"> <![endif]-->
+<html lang="en">
+<head>
+  
+  <!-- Basic -->
+  <title>Akzer Mobilya</title>
+  
+  <!-- Define Charset -->
+  <meta charset="utf-8">
+  
+  <!-- Responsive Metatag -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  
+  <!-- Page Description and Author -->
+  <meta name="description" content="Margo - Responsive HTML5 Template">
+  <meta name="author" content="iThemesLab">
+  
+  <!-- Bootstrap CSS  -->
+  <link rel="stylesheet" href="<?php echo SITE_FRONT_ASSETS_DISTCSS; ?>/bootstrap.min.css" type="text/css" media="screen">
+  
+  <!-- Font Awesome CSS -->
+  <link rel="stylesheet" href="<?php echo SITE_FRONT_ASSETS_DISTCSS; ?>/font-awesome.min.css" type="text/css" media="screen">
+
+  <!-- Revolution Banner CSS -->
+  <link rel="stylesheet" type="text/css" href="<?php echo SITE_FRONT_ASSETS_DISTCSS; ?>/settings.css" media="screen" />
+  
+  <!-- Margo CSS Styles  -->
+  <link rel="stylesheet" type="text/css" href="<?php echo SITE_FRONT_ASSETS_DISTCSS; ?>/style.css" media="screen">
+  
+  <!-- Responsive CSS Styles  -->
+  <link rel="stylesheet" type="text/css" href="<?php echo SITE_FRONT_ASSETS_DISTCSS; ?>/responsive.css" media="screen">
+  
+  <!-- Css3 Transitions Styles  -->
+  <link rel="stylesheet" type="text/css" href="<?php echo SITE_FRONT_ASSETS_DISTCSS; ?>/animate.css" media="screen">
+  
+  <!-- Color CSS Styles  -->
+  <link rel="stylesheet" type="text/css" href="<?php echo SITE_FRONT_ASSETS_DISTCSS; ?>/colors/red.css" title="red" media="screen" />
+  <link rel="stylesheet" type="text/css" href="<?php echo SITE_FRONT_ASSETS_DISTCSS; ?>/colors/jade.css" title="jade" media="screen" />
+  <link rel="stylesheet" type="text/css" href="<?php echo SITE_FRONT_ASSETS_DISTCSS; ?>/colors/blue.css" title="blue" media="screen" />
+  <link rel="stylesheet" type="text/css" href="<?php echo SITE_FRONT_ASSETS_DISTCSS; ?>/colors/beige.css" title="beige" media="screen" />
+  <link rel="stylesheet" type="text/css" href="<?php echo SITE_FRONT_ASSETS_DISTCSS; ?>/colors/cyan.css" title="cyan" media="screen" />
+  <link rel="stylesheet" type="text/css" href="<?php echo SITE_FRONT_ASSETS_DISTCSS; ?>/colors/green.css" title="green" media="screen" />
+  <link rel="stylesheet" type="text/css" href="<?php echo SITE_FRONT_ASSETS_DISTCSS; ?>/colors/orange.css" title="orange" media="screen" />
+  <link rel="stylesheet" type="text/css" href="<?php echo SITE_FRONT_ASSETS_DISTCSS; ?>/colors/peach.css" title="peach" media="screen" />
+  <link rel="stylesheet" type="text/css" href="<?php echo SITE_FRONT_ASSETS_DISTCSS; ?>/colors/pink.css" title="pink" media="screen" />
+  <link rel="stylesheet" type="text/css" href="<?php echo SITE_FRONT_ASSETS_DISTCSS; ?>/colors/purple.css" title="purple" media="screen" />
+  <link rel="stylesheet" type="text/css" href="<?php echo SITE_FRONT_ASSETS_DISTCSS; ?>/colors/sky-blue.css" title="sky-blue" media="screen" />
+  <link rel="stylesheet" type="text/css" href="<?php echo SITE_FRONT_ASSETS_DISTCSS; ?>/colors/yellow.css" title="yellow" media="screen" />
+  
+  
+  
+  <!-- Margo JS  -->
+  <script type="text/javascript" src="<?php echo SITE_FRONT_ASSETS_DISTJS; ?>/jquery-2.1.1.min.js"></script>
+  <script type="text/javascript" src="<?php echo SITE_FRONT_ASSETS_DISTJS; ?>/jquery.migrate.js"></script>
+  <script type="text/javascript" src="<?php echo SITE_FRONT_ASSETS_DISTJS; ?>/modernizrr.js"></script>
+  <script type="text/javascript" src="<?php echo SITE_FRONT_ASSETS_DISTJS; ?>/bootstrap.min.js"></script>
+  <script type="text/javascript" src="<?php echo SITE_FRONT_ASSETS_DISTJS; ?>/jquery.fitvids.js"></script>
+  <script type="text/javascript" src="<?php echo SITE_FRONT_ASSETS_DISTJS; ?>/owl.carousel.min.js"></script>
+  <script type="text/javascript" src="<?php echo SITE_FRONT_ASSETS_DISTJS; ?>/nivo-lightbox.min.js"></script>
+  <script type="text/javascript" src="<?php echo SITE_FRONT_ASSETS_DISTJS; ?>/jquery.isotope.min.js"></script>
+  <script type="text/javascript" src="<?php echo SITE_FRONT_ASSETS_DISTJS; ?>/jquery.appear.js"></script>
+  <script type="text/javascript" src="<?php echo SITE_FRONT_ASSETS_DISTJS; ?>/count-to.js"></script>
+  <script type="text/javascript" src="<?php echo SITE_FRONT_ASSETS_DISTJS; ?>/jquery.textillate.js"></script>
+  <script type="text/javascript" src="<?php echo SITE_FRONT_ASSETS_DISTJS; ?>/jquery.lettering.js"></script>
+  <script type="text/javascript" src="<?php echo SITE_FRONT_ASSETS_DISTJS; ?>/jquery.easypiechart.min.js"></script>
+  <script type="text/javascript" src="<?php echo SITE_FRONT_ASSETS_DISTJS; ?>/jquery.nicescroll.min.js"></script>
+  <script type="text/javascript" src="<?php echo SITE_FRONT_ASSETS_DISTJS; ?>/jquery.parallax.js"></script>
+  <script type="text/javascript" src="<?php echo SITE_FRONT_ASSETS_DISTJS; ?>/jquery.themepunch.plugins.min.js"></script>
+  <script type="text/javascript" src="<?php echo SITE_FRONT_ASSETS_DISTJS; ?>/jquery.themepunch.revolution.min.js"></script>
+  <script type="text/javascript" src="<?php echo SITE_FRONT_ASSETS_DISTJS; ?>/script.js"></script>
+
+  <!--[if IE 8]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+  <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+  
+</head>
+<body>
+
+	<!-- Container -->
+	<div id="container">
+		
+        <!-- Start Header -->
+        <div class="hidden-header"></div>
+        <header class="clearfix">
+          
+          <!-- Start Top Bar -->
+<!--          <div class="top-bar">
+            <div class="container">
+               <div class="row">-->
+<!--                  <div class="col-md-6">-->
+                    <!-- Start Contact Info -->
+<!--                    <ul class="contact-details">
+                                <li><a href="#"><i class="fa fa-map-marker"></i> House-54/A, London, UK</a>
+                                </li>
+                                <li><a href="#"><i class="fa fa-envelope-o"></i> info@yourcompany.com</a>
+                                </li>
+                                <li><a href="#"><i class="fa fa-phone"></i> +12 345 678 000</a>
+                                </li>
+                            </ul>-->
+                  <!-- End Contact Info -->
+<!--              </div>
+              <div class="col-md-6">
+                 Start Social Links 
+                <ul class="social-list">
+                  <li>
+                    <a class="facebook itl-tooltip" data-placement="bottom" title="Facebook" href="#"><i class="fa fa-facebook"></i></a>
+                </li>
+                <li>
+                    <a class="twitter itl-tooltip" data-placement="bottom" title="Twitter" href="#"><i class="fa fa-twitter"></i></a>
+                </li>
+                <li>
+                    <a class="google itl-tooltip" data-placement="bottom" title="Google Plus" href="#"><i class="fa fa-google-plus"></i></a>
+                </li>
+                <li>
+                    <a class="dribbble itl-tooltip" data-placement="bottom" title="Dribble" href="#"><i class="fa fa-dribbble"></i></a>
+                </li>
+                <li>
+                    <a class="linkdin itl-tooltip" data-placement="bottom" title="Linkedin" href="#"><i class="fa fa-linkedin"></i></a>
+                </li>
+                <li>
+                    <a class="flickr itl-tooltip" data-placement="bottom" title="Flickr" href="#"><i class="fa fa-flickr"></i></a>
+                </li>
+                <li>
+                    <a class="tumblr itl-tooltip" data-placement="bottom" title="Tumblr" href="#"><i class="fa fa-tumblr"></i></a>
+                </li>
+                <li>
+                    <a class="instgram itl-tooltip" data-placement="bottom" title="Instagram" href="#"><i class="fa fa-instagram"></i></a>
+                </li>
+                <li>
+                    <a class="vimeo itl-tooltip" data-placement="bottom" title="vimeo" href="#"><i class="fa fa-vimeo-square"></i></a>
+                </li>
+                <li>
+                    <a class="skype itl-tooltip" data-placement="bottom" title="Skype" href="#"><i class="fa fa-skype"></i></a>
+                </li>
+            </ul>
+             End Social Links 
+        </div>-->
+<!--    </div>
+</div>
+</div>-->
+<!-- End Top Bar -->
+
+<!-- Start Header ( Logo & Naviagtion ) -->
+<div class="navbar navbar-default navbar-top">
+  <div class="container">
+     <div class="navbar-header">
+        <!-- Stat Toggle Nav Link For Mobiles -->
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+           <i class="fa fa-bars"></i>
+       </button>
+       <!-- End Toggle Nav Link For Mobiles -->
+       <a class="navbar-brand" href="index.html"><img alt="" src="<?php echo SITE_FRONT_ASSETS_DISTIMG; ?>/margo.png"></a>
+   </div>
+   <div class="navbar-collapse collapse">
+      <!-- Stat Search -->
+      
+<!-- End Search -->
+<!-- Start Navigation List -->
+<ul class="nav navbar-nav navbar-right">
+ <li>
+    <a class="active" href="index.html">ANASAYFA</a>
+   
+</li>
+<li>
+  <a href="about.html">HAKKIMIZDA</a>
+  
+</li>
+<li>
+    <a href="#">ÜRÜNLRİMİZ</a>
+    <ul class="dropdown">
+      <li><a href="tabs.html">Mutfak Dolabı</a></li>
+      <li><a href="buttons.html">Yatak Odası Dolabı</a></li>
+      <li><a href="action-box.html">Vestiyer</a></li>
+      <li><a href="testimonials.html">Şark Köşesi</a></li>
+      <li><a href="latest-posts.html">Alüminyum Doğrama</a></li>
+      <li><a href="latest-projects.html">Demir Doğrama</a></li>
+      <li><a href="pricing.html">Panel Kapı</a></li>
+      <li><a href="animated-graphs.html">Ahşap Kapı</a></li>
+      <li><a href="accordion-toggles.html">Laminat Parke</a></li>
+  </ul>
+</li>
+<li>
+    <a href="portfolio-3.html">İLETİŞİM</a>
+    
+</li>
+
+</ul>
+<!-- End Navigation List -->
+</div>
+</div>
+</div>
+<!-- End Header ( Logo & Naviagtion ) -->
+
+</header>
+<!-- End Header -->
